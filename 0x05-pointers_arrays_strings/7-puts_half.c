b4df6782693da491fe051deb55e82e4f3db491c0
sara yamil <sarayamil18@gmail.com>
2023-05-02 07:41:34 -0700

    idoz 0-main.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,16 +9,16 @@ Return: 0 is success
 */
 void puts_half(char *str)
 {
-int string = 0, n, i;
+	int string = 0, n, i;
 
-while (str[string] != '\0')
-string++;
-if (string % 2 == 0)
-n = string / 2;
-else
-n = (string + 1) / 2;
+	while (str[string] != '\0')
+		string++;
+	if (string % 2 == 0)
+		n = string / 2;
+	else
+	n = (string + 1) / 2;
 
-for (i = n; i < string; i++)
-_putchar(str[i]);
-_putchar('\n');
+	for (i = n; i < string; i++)
+	_putchar(str[i]);
+	_putchar('\n');
 }
