3f8beb1c318686a56ffe95e7e0f2350b3fb45128
sara yamil <sarayamil18@gmail.com>
2023-05-02 06:48:50 -0700

    idoz 0-main.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  reset_to_98  takes the pointer updates the value
+ *  reset_to_98 - takes the pointer and updates the value
  *  @n: int to check
- *  Owned By SARA
- *  Return: 0 
+ *  Owned By Bwave/Bright
+ *  Return: 0 is success
  */
 
 void	reset_to_98(int *n)
