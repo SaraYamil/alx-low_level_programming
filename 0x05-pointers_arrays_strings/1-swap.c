9c41992871c710d067348737754c9309cd2c56e4
sara yamil <sarayamil18@gmail.com>
2023-05-02 06:52:56 -0700

    idoz 0-main.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+ii#include "main.h"
+
+/**
+ * swap_int - tanbdlo l  value dyal int a b int b
+ * Owned by sun/moon
+ * @a: awal int
+ * @b: tani int
+ * Return 0 rah t9ad
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
