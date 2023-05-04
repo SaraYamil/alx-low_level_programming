6db0db90a4e5a909cd3d7e1701c1ceaa29239ac2
sara yamil <sarayamil18@gmail.com>
2023-05-04 08:32:00 -0700

    wa ha l3333ar

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int main(void)
 {
   int n;
@@ -6,13 +8,13 @@ int main(void)
 
   a[2] = 1024;
   p = &n;
-   /*
-   * nkatbo chi haja hna
+  /*
+   * write your line of code here...
    * Remember:
-   * - inaa homa sakin ntoma
-   * - l3ar ya l3ar
-   * - wrini bab
-   * - darkom njikom
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
    */
   *(p + 5) = 98;
   /* ...so that this prints 98\n */
