120dd5e962a45e2a297678efd51c61a3b3e50fd8
sara yamil <sarayamil18@gmail.com>
2023-05-08 06:55:19 -0700

    wach a alx cv

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+i#include "main.h"
+/**
+*_memcpy - c katcopy misaha
+*@dest: vriable lwal 
+*@src: mimor li ancopyh
+*@n: 3dad abyat
+*Return: maso9kch
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+  *(dest + i) = *(src + i);
+}
+return (dest);
+}
