18af2b158bc43f73f0a4c3e560a9fb98dc7f5e49
sara yamil <sarayamil18@gmail.com>
2023-05-08 07:11:01 -0700

    by a alx

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- * _memcpy - fonction fchkel
- *
- * @dest: ach ghadi ncopiw
- * @src: a la hadi hya ancopiw ama lwala rah fin ancopiw
- * @n: 3dad bytes
- *
- * Return: maso9akch
+*_memcpy - man3erf
+*@dest: pointer value
+*@src: awdi ana soqi
+*@n: nmuero byte
+*Return: maso9akch
 */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-  unsigned int i;
+unsigned int i;
 
-  for (i = 0; i < n; i++)
-    dest[i] = src[i];
-  return (dest);
+
+for (i = 0; i < n; i++)
+{
+  *(dest + i) = *(src + i);
+}
+return (dest);
 }
