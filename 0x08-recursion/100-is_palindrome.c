1c86ce4d42e73dd403d62c6af7f49eb162010d7b
sara yamil <sarayamil18@gmail.com>
2023-05-10 04:23:25 -0700

    sara fara

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,42 +4,45 @@ int check_pal(char *s, int i, int len);
 int _strlen_recursion(char *s);
 
 /**
- * is_palindrome - fonction string palindrome
- * @s: stringy
- * Return: hal3ar
+ * is_palindrome - fonction string is a palindrome
+ * @s: stringy w safe
+ *
+ * Return: maso9akch
  */
 int is_palindrome(char *s)
 {
-    if (*s == 0)
-        return (1);
-    return (check_pal(s, 0, _strlen_recursion(s)));
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
- * _strlen_recursion - tol ta3 string
- * @s: vriable
- * Return: hal3ar
+ * _strlen_recursion - length of a string
+ * @s: string  length of
+ *
+ * Return: dwzo lina za3ef
  */
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0')
-        return (0);
-    return (1 + _strlen_recursion(s + 1));
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
  * check_pal - fonction characters recursively for palindrome
- * @s: stringy
- * @i: int
- * @len: tol ta3 string
+ * @s: stringy w safe
+ * @i: varibale
+ * @len: tol ta3 l3iba
  *
- * Return: hal3ar
+ * Return: 1 if palindrome, 0 if not
  */
 int check_pal(char *s, int i, int len)
 {
-    if (*(s + i) != *(s + len - 1))
-        return (0);
-    if (i >= len)
-        return (1);
-    return (check_pal(s, i + 1, len - 1));
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
+
