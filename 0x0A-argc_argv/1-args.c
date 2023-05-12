9b9435962dd758a14f4117b730552c84146f5839
sara yamil <sarayamil18@gmail.com>
2023-05-12 05:38:11 -0700

    baz

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - printy ar9am ta3 l'argg f program
+ * @argc: number  arguments
+ * @argv: array  arguments
+ *
+ * Return: mso9ekch
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv; /*Ignore argv*/
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
