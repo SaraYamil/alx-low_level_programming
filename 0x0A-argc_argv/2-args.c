7196f4bb1ccf0da800b5490960522264fcb3d711
sara yamil <sarayamil18@gmail.com>
2023-05-12 05:50:14 -0700

    hi

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Program argv
+ *
+ * @argc: Size argv
+ * @argv: Array  size argc
+ *
+ * Return: mso9kech
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
