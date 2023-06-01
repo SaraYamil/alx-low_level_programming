9271d6571e47664a1952f012e383d105cbb72b21
sara yamil <sarayamil18@gmail.com>
2023-06-01 12:59:39 -0700

    sara

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * free_list - fonction  code
+ * @head: iinpute pointer
+ * Return: maso9akch a kimba.
+ */
+void free_list(list_t *head)
+{
+	list_t *nv, *nv2;
+
+	nv = head;
+	while (nv)
+	{
+		free(nv->str);
+		nv2 = nv->next;
+		free(nv);
+		av = nv2;
+	}
+}
