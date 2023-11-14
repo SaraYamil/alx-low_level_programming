b0b6ab622725d117cc03f62746ff6b916f20e56c
root <sarayamil18@gmail.com>
2023-11-14 11:49:58 -0800

    sara

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,47 +1,66 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "search_algos.h"
 
 /**
- * binary_search - function
- * @array: desc
- * @size: desc
- * @value: desc
+ * binary_search - searches for a value in a sorted array of integers using the
+ *                Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in the array
+ * @value: value to search for
  *
- * Return: Return
- */
+ * Return: first index where value is located, or -1 if value is not present
+ *       in array or if array is NULL
+*/
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left = 0;
-	size_t right = size - 1;
-	size_t middle;
-	size_t i;
+	int i = recursive_search(array, size, value);
 
-	if (!array)
-		return (-1);
-
-	while (left <= right)
+	if (i >= 0 && array[i] != value)
 	{
-		middle = left + (right - left) / 2;
-
-		printf("Searching in array: ");
-		for (i = left; i <= right; i++)
-		{
-			printf("%d", array[i]);
-			if (i < right)
-				printf(", ");
-			else
-				printf("\n");
-		}
-
-		if (array[middle] == value)
-			return (middle);
-
-		if (array[middle] < value)
-			left = middle + 1;
-		else
-			right = middle - 1;
+		return (-1);
 	}
 
-	return (-1);
+	return (i);
+}
+
+/**
+ * recursive_search - searches for a value in a sorted array of integers using
+ *                   the Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in the array
+ * @value: value to search for
+ *
+ * Return: first index where value is located, or -1 if value is not present
+ *      in array or if array is NULL
+*/
+int recursive_search(int *array, size_t size, int value)
+{
+	size_t h = size / 2, i;
+
+	if (array == NULL || size == 0)
+	{
+		return (-1);
+	}
+	printf("Searching in array");
+	for (i = 0; i < size; i++)
+	{
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+	}
+	printf("\n");
+
+	if (h && size % 2 == 0)
+	{
+		h--;
+	}
+
+	if (value == array[h])
+	{
+		return ((int)h);
+	}
+	if (value < array[h])
+	{
+		return (recursive_search(array, h, value));
+	}
+	h++;
+
+	return (recursive_search(array + h, size - h, value) + h);
 }
